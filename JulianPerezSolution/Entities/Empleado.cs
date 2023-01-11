06df98741fadf4c9ad45708c6e5e4e37313b0290
Julian Perez Demonty <jperezdemonty@live.com.ar>
2023-01-11 17:30:58 -0300

    Creacion del proyecto

---
+++ JulianPerezSolution/Entities/Empleado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Empleado: Entity
+    {
+        public string Codigo { get; set; }
+        public string Apellido { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaAlta { get; set; }
+        public int IdTipoDto { get; set; }
+        public int NumDocumento { get; set; }
+    }
+}
